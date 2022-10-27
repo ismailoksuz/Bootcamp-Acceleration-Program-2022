71e018854e01c3cfa56b020a910f841c446b2a60
İsmail Öksüz <46068969+ismailoksuz@users.noreply.github.com>
2022-10-28 02:05:31 +0300

    Update Ucgen-Kenarı-Hesaplama.java

--- Ucgen-Kenarı-Hesaplama.java
+++ Ucgen-Kenarı-Hesaplama.java
@@ -3,19 +3,20 @@ public class Main
 {
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
-		double kenar=0;
-		System.out.println("2 Kenarının uzunluğu verilen üçgenin 3. kenarının uzunluğunu bulma programına hoşgeldiniz.");
-		System.out.println("Yapmak istediğiniz işlemi seçiniz: (A veya B'ye basarak seçim yapabilirsiniz)");
+		double sonuc=0;
+		System.out.println("Yapmak istediğiniz işlemi seçiniz: (A,B,C'ye basarak seçim yapabilirsiniz)");
 		System.out.println("A - Hipotenüsü ve bir kenarı verilen üçgenin diğer dik kenarını hesaplamak.");
 		System.out.println("B - 2 dik kenarı verilen üçgenin hipotenüs uzunluğunu hesaplamak.");
+		System.out.println("C - 3 kenarı verilen üçgenin alanını hesaplamak.");
 		char inp=scan.next().charAt(0);
-		if(inp=='A')kenar=kenarBul();
-		else if(inp=='B')kenar=hipotenusBul();
+		if(inp=='A')sonuc=kenarBul();
+		else if(inp=='B')sonuc=hipotenusBul();
+		else if(inp=='C')sonuc=alanHesapla();
 		else{
 		    System.out.println("Geçersiz değer girdiniz.");
 		    System.exit(0);
 		}
-		System.out.print("Sonuç: "+kenar);
+		System.out.print("Sonuç: "+sonuc);
 	}
 	public static double hipotenusBul(){
 	    Scanner scan=new Scanner(System.in);
@@ -33,6 +34,16 @@ public class Main
 	    double hipotenus=scan.nextDouble();
 	    double kenar2=Math.sqrt((hipotenus*hipotenus)-(kenar1*kenar1));
 	    return kenar2;
+	}
+	public static double alanHesapla(){
+	    Scanner scan=new Scanner(System.in);
+	    System.out.print("Kenarların uzunluklarını giriniz:");
+	    double a=scan.nextDouble();
+	    double b=scan.nextDouble();
+	    double c=scan.nextDouble();
+	    double u=(a+b+c)/2;
+	    double sonuc=Math.sqrt(u*(u-a)*(u-b)*(u-c));
+	    return sonuc;
 	}
 	
 }
