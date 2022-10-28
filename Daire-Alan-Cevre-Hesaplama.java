71db34d1c5662af295600e3c0155ea51c55d3e8b
İsmail Öksüz <46068969+ismailoksuz@users.noreply.github.com>
2022-10-28 16:21:51 +0300

    Update Daire-Alan-Cevre-Hesaplama.java

--- Daire-Alan-Cevre-Hesaplama.java
+++ Daire-Alan-Cevre-Hesaplama.java
@@ -18,21 +18,41 @@ public class Main
 		double r=scan.nextDouble();
 		double sonuc=0;
 		double aci;
-		if(islem==1)sonuc=2*pi*r;
-		else if(islem==2)sonuc=pi*r*r;
+		if(islem==1)sonuc=CevreHesapla(r,pi);
+		else if(islem==2)sonuc=AlanHesapla(r,pi);
 		else if(islem==3){
 		    System.out.print("Merkez açının derecesini giriniz:");
 		    aci=scan.nextDouble();
-		    sonuc=(2*pi*r*aci)/360;
+		    if(aci>360 || aci<0){
+		        System.out.print("Geçersiz değer girdiniz");
+		        System.exit(0);
+		    }
+		    sonuc=CevreParcaHesapla(r,pi,aci);
 		}
 		else{
 		    System.out.print("Merkez açının derecesini giriniz:");
 		    aci=scan.nextDouble();
-		    sonuc=(pi*r*r*aci)/360;
+		    if(aci>360 || aci<0){
+		        System.out.print("Geçersiz değer girdiniz");
+		        System.exit(0);
+		    }
+		    sonuc=AlanParcaHesapla(r,pi,aci);
 		}
 		System.out.print("Sonuç: "+sonuc);
 		
 		
+	}
+	public static double AlanHesapla (double r,double pi){
+	    return pi*r*r;
+	}
+	public static double CevreHesapla (double r,double pi){
+	    return 2*pi*r;
+	}
+	public static double AlanParcaHesapla (double r,double pi,double aci){
+	    return (pi*r*r*aci)/360;
+	}
+	public static double CevreParcaHesapla (double r,double pi,double aci){
+	    return (2*pi*r*aci)/360;
 	}
 	
 }
